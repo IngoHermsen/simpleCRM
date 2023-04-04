3105048db108f4b38f8963389c7383641f09807d
Ingo Hermsen <ingohermsen@gmail.com>
2023-04-04 21:29:49 +0200

    added router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'users'}
+  {path: 'user', component: UserComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
